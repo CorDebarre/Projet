97e9067713bedc5e0fedae024b1b382b5dcc750e
Corentin <Corentin@DESKTOP-EJ2ME2N>
2019-03-26 14:16:19 +0100

    Commit 1

---
+++ ESEO_API_REST/src/main/java/com/ESEO_API_REST/App.java
@@ -0,0 +1,23 @@
+package com.ESEO_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+//@RequestMapping("/path")
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try {
+			SpringApplication.run(App.class,args);
+			System.out.println("Application démarrée ");
+		}catch (Exception e) {
+			System.out.println("Application pas démarrée " + e);
+		}
+    }
+}
